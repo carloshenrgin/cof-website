325dfd027c9500d2a242a581318d358ffa4a0b29
Carlos Henrique <carloshenrgin@ufu.br>
2022-01-16 06:04:45 -0300

    Added Gatsby Image plugin to project. Added hero section html

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,41 @@
 import React from "react"
+import { StaticImage } from "gatsby-plugin-image"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const landingPage = () => {
+  return (
+    <header class="hero-section">
+      <nav>
+        <StaticImage
+          src="../images/cof-logo-cropped-compressed.png"
+          alt="Contra o Filé Logo"
+          class="nav-logo"
+        />
+
+        <ul class="main-nav">
+          <li>
+            <a href="#">Cardápio</a>
+          </li>
+          <li>
+            <a href="#">Galeria</a>
+          </li>
+          <li>
+            <a href="#">Sobre Nós</a>
+          </li>
+          <li>
+            <a href="#">Contato</a>
+          </li>
+        </ul>
+      </nav>
+
+      <div class="hero-textbox">
+        <h1>Saudável. Delicioso. Vegano,</h1>
+        <p>Uma opção saudável e acessível para todos.</p>
+        <a class="btn" href="#">
+          Tô com fome
+        </a>
+      </div>
+    </header>
+  )
 }
+
+export default landingPage
